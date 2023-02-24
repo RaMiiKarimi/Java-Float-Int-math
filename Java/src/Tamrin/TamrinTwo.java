a3763a6a6f1088d013575596b6332106246b4e07
RaMii <89271443+RaMiiOffiCial@users.noreply.github.com>
2023-02-24 22:23:36 +0330

    Add files via upload

---
+++ Java/src/Tamrin/TamrinTwo.java
@@ -0,0 +1,71 @@
+package Tamrin;
+
+public class TamrinTwo {
+
+	public static void main(String[] args) {
+		
+		
+//Integers
+int x=5;
+int x1=3;
+
+
+// + , - , / , *
+int Jame= x+x1;
+int Tafrigh = x-x1;
+int Taghsim =  x / x1;
+int Zarb= x * x1;
+	
+	System.out.println("Jame Tafrigh Taghsim Va Zard Addad Ebarat And Az  : ");
+	
+	System.out.println("Jame Do Adad Int Shoma : " + Jame);
+	
+	System.out.println("Tafigh Do Adad Int Shoma : " + Tafrigh);
+//Albate Taghsim Toye Int Ha Round Mishe Ino Bayd Ba Float Moshakhas konid Ta Adad Aslio Bede Yani 1.6666
+	
+	System.out.println("Taghsim Do Adad Int Shoma : " + Taghsim);
+	
+	System.out.println("Zarb Do Adad Int Shoma  : " + Zarb);
+	
+	System.out.println("-------------------------------------------------------------------------");
+	
+	
+//Floats	
+
+float c = 5.5f;
+float b = 10.5f;
+// + , - , / , *
+float Jamecb = c + b;
+float Tafrighcb = c - b;
+float Taghsimcb = c / b;
+float Zarbcb= c * b;
+	
+	System.out.println("Jame Adad Float Shoma : " +  Jamecb);
+	
+	System.out.println("Tafrigh Adad Float Shoma : " + Tafrighcb);
+	
+	System.out.println("Taghsim Adad Float Shoma : " + Taghsimcb);
+	
+	System.out.println("Zarb Adad Float Shoma : " + Zarbcb);
+	
+	System.out.println("--------------------------------------------------------------------------");
+	
+	
+//Float And Integer
+float JameFI= c + x;
+float TafrighFI = c - x;
+float TaghsimFI = c / x;
+float ZarbFI = c * x;
+
+// + , - , / , *
+	System.out.println("Jame Do Adad Float Va Int Shoma : " + JameFI);
+	System.out.println("Tafrigh Do Adad Float Va Int Shoma :" + TafrighFI);
+	System.out.println("Taghsim Do Adad Float Va Int Shoma : " + TaghsimFI);
+	System.out.println("Zarb Do Adad Float Va Int Shoma : " + ZarbFI);
+
+	
+System.out.println("Movafaq Bashid \n#Ramtin Karimi");
+
+	}
+
+}
